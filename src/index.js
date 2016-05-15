6ef3e2b0913c795acc28b95a2f6d50a28596b04e
Richard <hello@noxan.net>
2016-05-15 21:31:59 +0200

    setup webpack with revealjs

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('reveal.js/css/reveal.css');
+require('reveal.js/css/theme/white.css');
+
+
+var Reveal = require('reveal.js');
+
+Reveal.initialize();
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: 'dist',
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    root: path.resolve(__dirname, 'src'),
+    extensions: ['', '.js'],
+  },
+  module: {
+    preLoaders: [
+    ],
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&minetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file' },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  devtool: 'source-map',
+};
